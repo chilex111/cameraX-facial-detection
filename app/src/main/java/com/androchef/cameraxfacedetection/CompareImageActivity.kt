1e2bec3dfab16ba2a435cf15fc27ac275f4886df
Chilex <Chidinma@initsng.com>
2022-05-12 16:07:21 +0100

    add implement open_cv

--- app/src/main/java/com/androchef/cameraxfacedetection/CompareImageActivity.kt
+++ app/src/main/java/com/androchef/cameraxfacedetection/CompareImageActivity.kt
@@ -1,6 +1,7 @@
 package com.androchef.cameraxfacedetection
 
 import android.content.Intent
+import android.content.res.AssetFileDescriptor
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
@@ -13,7 +14,19 @@ import com.androchef.cameraxfacedetection.GalleryImageActivity.Companion.GALLERY
 import com.androchef.cameraxfacedetection.camerax.CameraManager.Companion.IMAGE_URI_SAVED
 import kotlinx.android.synthetic.main.activity_compare_image.*
 import org.opencv.android.OpenCVLoader
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.Interpreter
+import org.tensorflow.lite.gpu.GpuDelegate
+import org.tensorflow.lite.support.common.ops.NormalizeOp
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.io.FileInputStream
+import java.io.IOException
 import java.nio.MappedByteBuffer
+import java.nio.channels.FileChannel
+import kotlin.math.sqrt
 
 
 class CompareImageActivity : AppCompatActivity() {
@@ -47,6 +60,8 @@ class CompareImageActivity : AppCompatActivity() {
                 Log.i(TAG, "opencv not installed");
             }
       //  }
+        initializeModel()
+
         imageView1.layoutParams.height = 400
         imageView2.layoutParams.height = 400
 
@@ -99,12 +114,6 @@ class CompareImageActivity : AppCompatActivity() {
         popupMenu.show()
     }
 
-/*
-    private fun openGallery(id: Int) {
-        val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
-        startActivityForResult(gallery, id)
-    }*/
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == RESULT_OK) {
@@ -139,6 +148,93 @@ class CompareImageActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    private fun initializeModel() {
+        try {
+            tfliteModel = loadModelFile()
+
+            val delegate = GpuDelegate(GpuDelegate.Options().setQuantizedModelsAllowed(true))
+            val options = (Interpreter.Options()).addDelegate(delegate)
+
+            @Suppress("DEPRECATION")
+            interpreter = Interpreter(tfliteModel, options)
+
+            val probabilityTensorIndex = 0
+            val probabilityShape =
+                interpreter.getOutputTensor(probabilityTensorIndex).shape() // {1, EMBEDDING_SIZE}
+            val probabilityDataType = interpreter.getOutputTensor(probabilityTensorIndex).dataType()
+
+            // Creates the input tensor
+            tImage = TensorImage(DataType.FLOAT32)
+
+            // Creates the output tensor and its processor
+            tBuffer = TensorBuffer.createFixedSize(probabilityShape, probabilityDataType)
+
+            Log.d(TAG, "Model loaded successful")
+        } catch (e: IOException) {
+            Log.e(TAG, "Error reading model", e)
+        }
+    }
+
+    private fun loadModelFile(): MappedByteBuffer {
+        val fileDescriptor: AssetFileDescriptor = assets.openFd(MODEL_PATH)
+        val inputStream = FileInputStream(fileDescriptor.fileDescriptor)
+        val fileChannel: FileChannel = inputStream.channel
+        val startOffset: Long = fileDescriptor.startOffset
+        val declaredLength: Long = fileDescriptor.declaredLength
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength)
+    }
+
+    private fun recognize(embedding1: FloatArray?, embedding2: FloatArray?): String {
+        if (embedding1 != null || embedding2 != null) {
+            return if (embedding1!!.isNotEmpty() || embedding2!!.isNotEmpty()) {
+                val maxVal = cosineSimilarity(embedding1, embedding2)
+                Log.e(TAG, maxVal.toString())
+                if (maxVal > 0.50) " ${(maxVal * 100).toString().take(2)}%"
+                else "unknown:: ${(maxVal * 100)}%"
+            } else "unknown"
+        }
+        return "unknown: 0%"
+    }
+
+    private fun getEmbedding(bitmap: Bitmap): FloatArray? {
+        tImage = loadImage(bitmap)
+
+        interpreter.run(tImage.buffer, tBuffer?.buffer?.rewind())
+
+        return tBuffer?.floatArray
+    }
+
+    private fun loadImage(bitmap: Bitmap): TensorImage {
+        // Loads bitmap into a TensorImage
+        tImage.load(bitmap)
+
+        val imageProcessor = ImageProcessor.Builder()
+            .add(ResizeOp(inputImageHeight, inputImageWidth, ResizeOp.ResizeMethod.BILINEAR))
+            .add(NormalizeOp(IMAGE_MEAN, IMAGE_STD))
+            .build()
+        return imageProcessor.process(tImage)
+    }
+
+    private fun cosineSimilarity(A: FloatArray?, B: FloatArray?): Float {
+        if (A == null || B == null || A.isEmpty() || B.isEmpty() || A.size != B.size) {
+            return 2.0F
+        }
+
+        var sumProduct = 0.0
+        var sumASq = 0.0
+        var sumBSq = 0.0
+        for (i in A.indices) {
+            sumProduct += (A[i] * B[i]).toDouble()
+            sumASq += (A[i] * A[i]).toDouble()
+            sumBSq += (B[i] * B[i]).toDouble()
+        }
+        val sumTotal = if (sumASq == 0.0 && sumBSq == 0.0) {
+            2.0F
+        } else (sumProduct / (sqrt(sumASq) * sqrt(sumBSq))).toFloat()
+        Log.e(TAG, sumTotal.toString())
+        return sumTotal
     }
 
     companion object {
