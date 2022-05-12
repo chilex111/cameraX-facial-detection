fbef4123725371606641e9f90b306054728527b9
Chilex <Chidinma@initsng.com>
2022-05-12 14:35:42 +0100

    face detection with open cv is working perfectly

--- app/src/main/java/com/androchef/cameraxfacedetection/GalleryImageActivity.kt
+++ app/src/main/java/com/androchef/cameraxfacedetection/GalleryImageActivity.kt
@@ -44,8 +44,7 @@ class GalleryImageActivity : AppCompatActivity() {
                 intent.putExtra(GALLERY_URI, saveUri.toString())
                 setResult(RESULT_OK, intent)
                 finish()
-            }
-            else{
+            } else {
                 Toast.makeText(this, "Face possibly not found", Toast.LENGTH_LONG).show()
             }
         }
@@ -55,7 +54,8 @@ class GalleryImageActivity : AppCompatActivity() {
         val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
         startActivityForResult(gallery, PICK_IMAGE)
     }
-    private fun getImageUri( inImage: Bitmap): Uri? {
+
+    private fun getImageUri(inImage: Bitmap): Uri? {
         val bytes = ByteArrayOutputStream()
         inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
         val path = MediaStore.Images.Media.insertImage(
@@ -66,12 +66,13 @@ class GalleryImageActivity : AppCompatActivity() {
         )
         return Uri.parse(path)
     }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == RESULT_OK) {
             imageUri = data?.data
 
-           // imageView.setImageURI(imageUri)
+            // imageView.setImageURI(imageUri)
             //imageUri?.let { detectFace(it) }
             val realPath = imageUri?.let { getRealPathFromUri(it) }
             val src = Imgcodecs.imread(realPath)
@@ -79,8 +80,9 @@ class GalleryImageActivity : AppCompatActivity() {
             detectFaceOpenCV(src)
         }
     }
+
     fun Context.getRealPathFromUri(uri: Uri): String {
-        var picturePath=""
+        var picturePath = ""
         val filePathColumn = arrayOf(MediaStore.Files.FileColumns.DATA)
         val cursor: Cursor? = contentResolver.query(
             uri, filePathColumn,
@@ -95,45 +97,43 @@ class GalleryImageActivity : AppCompatActivity() {
         }
         return picturePath
     }
-/*
-    private fun detectFace(imageUri: Uri) {
-        val imageBitmap = if (Build.VERSION.SDK_INT < 28) {
-            MediaStore.Images.Media.getBitmap(contentResolver, imageUri)
-        } else {
-            val source =
-                ImageDecoder.createSource(contentResolver, imageUri)
-            ImageDecoder.decodeBitmap(source)
-        }
-       val bitmap = imageBitmap.copy(Bitmap.Config.ARGB_8888, true)// convert image from hardware bitmap to software bitmap :(
-        val paint = Paint()
-        paint.strokeWidth = 6f
-        paint.color = Color.RED
-        paint.style = Paint.Style.STROKE
-        tempBitmap = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.RGB_565)
-        val canvas = Canvas(tempBitmap!!)
-        canvas.drawBitmap(bitmap, 0f, 0f, null)
-        val faceDetector: FaceDetector = FaceDetector.Builder(this).setTrackingEnabled(false)
-            .build()
-        if (!faceDetector.isOperational) {
-            AlertDialog.Builder(this).setMessage("Could not set up Face Detector!").show()
-            return
-        }
-        val frame: Frame = Frame.Builder().setBitmap(bitmap).build()
-        val faces: SparseArray<Face> = faceDetector.detect(frame)
-        for (i in 0 until faces.size()) {
-            val thisFace: Face = faces.valueAt(i)
-            val x1: Float = thisFace.position.x
-            val y1: Float = thisFace.position.y
-            val x2: Float = x1 + thisFace.width
-            val y2: Float = y1 + thisFace.height
-            canvas.drawRoundRect(RectF(x1, y1, x2, y2), 2f, 2f, paint)
-        }
-        imageView.setImageDrawable(BitmapDrawable(resources, tempBitmap))
-    }*/
+
+    /*
+        private fun detectFace(imageUri: Uri) {
+            val imageBitmap = if (Build.VERSION.SDK_INT < 28) {
+                MediaStore.Images.Media.getBitmap(contentResolver, imageUri)
+            } else {
+                val source =
+                    ImageDecoder.createSource(contentResolver, imageUri)
+                ImageDecoder.decodeBitmap(source)
+            }
+           val bitmap = imageBitmap.copy(Bitmap.Config.ARGB_8888, true)// convert image from hardware bitmap to software bitmap :(
+            val paint = Paint()
+            paint.strokeWidth = 6f
+            paint.color = Color.RED
+            paint.style = Paint.Style.STROKE
+            tempBitmap = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.RGB_565)
+            val canvas = Canvas(tempBitmap!!)
+            canvas.drawBitmap(bitmap, 0f, 0f, null)
+            val faceDetector: FaceDetector = FaceDetector.Builder(this).setTrackingEnabled(false)
+                .build()
+            if (!faceDetector.isOperational) {
+                AlertDialog.Builder(this).setMessage("Could not set up Face Detector!").show()
+                return
+            }
+            val frame: Frame = Frame.Builder().setBitmap(bitmap).build()
+            val faces: SparseArray<Face> = faceDetector.detect(frame)
+            for (i in 0 until faces.size()) {
+                val thisFace: Face = faces.valueAt(i)
+                val x1: Float = thisFace.position.x
+                val y1: Float = thisFace.position.y
+                val x2: Float = x1 + thisFace.width
+                val y2: Float = y1 + thisFace.height
+                canvas.drawRoundRect(RectF(x1, y1, x2, y2), 2f, 2f, paint)
+            }
+            imageView.setImageDrawable(BitmapDrawable(resources, tempBitmap))
+        }*/
     private fun detectFaceOpenCV(src: Mat) {
-
-        // Detecting the face in the snap
-
         // Detecting the face in the snap
         val faceDetections = MatOfRect()
         faceDetector?.detectMultiScale(src, faceDetections)
@@ -143,27 +143,41 @@ class GalleryImageActivity : AppCompatActivity() {
                 faceDetections.toArray().size
             )
         )
-
-        // Drawing boxes
-
         // Drawing boxes
         for (rect in faceDetections.toArray()) {
+            face = rect
             Imgproc.rectangle(
-               src,  // where to draw the box
+                src,  // where to draw the box
                 Point(rect.x.toDouble(), rect.y.toDouble()),  // bottom left
-                Point(rect.x + rect.width.toDouble(), rect.y + rect.height.toDouble()),  // top right
+                Point(
+                    rect.x + rect.width.toDouble(),
+                    rect.y + rect.height.toDouble()
+                ),  // top right
                 Scalar(0.0, 0.0, 255.0),
                 1 // RGB colour
             )
         }
-    tempBitmap = convertMatToBitMap(src)
-    imageView.setImageBitmap(tempBitmap)
+
+        val tempsBitmap = convertMatToBitMap(src)
+        if (tempsBitmap != null) {
+            tempBitmap = Bitmap.createBitmap(
+                tempsBitmap,
+                face.x,
+                face.y,
+                face.width,
+                face.height
+            )
+            imageView.setImageBitmap(tempBitmap)
+        }
     }
+
+    private lateinit var face: Rect
     private fun loadFaceLib() {
         try {
             val modelInputStream =
                 resources.openRawResource(
-                    R.raw.haarcascade_frontalface_alt2)
+                    R.raw.haarcascade_frontalface_alt2
+                )
 
             // create a temp directory
             faceDir = getDir(FACE_DIR, MODE_PRIVATE)
@@ -188,9 +202,10 @@ class GalleryImageActivity : AppCompatActivity() {
 
             faceDetector = CascadeClassifier(faceModel.absolutePath)
         } catch (e: IOException) {
-            Log.e("Error ","loading cascade face model...$e")
+            Log.e("Error ", "loading cascade face model...$e")
         }
     }
+
     private fun convertMatToBitMap(input: Mat): Bitmap? {
         var bmp: Bitmap? = null
         val rgb = Mat()
@@ -203,9 +218,11 @@ class GalleryImageActivity : AppCompatActivity() {
         }
         return bmp
     }
+
     companion object {
         private const val PICK_IMAGE = 1
         const val GALLERY_URI = "image_frm_gallery"
+
         // Face model
         private const val FACE_DIR = "facelib"
         private const val FACE_MODEL = "haarcascade_frontalface_alt2.xml"
